1cae8d2e550ac6afd1990be2849513e56bc6446b
Elad Kehat <eladkehat@gmail.com>
2019-04-08 22:38:24 +0300

    Code comments on the generate function.

--- baconator/baconator.py
+++ baconator/baconator.py
@@ -3,7 +3,21 @@ import random
 from baconator.names import FIRST_NAMES, LAST_NAMES
 
 
-def generate(delimiter: str = '-', token_len: int = 4):
+def generate(delimiter: str = '-', token_len: int = 4) -> str:
+    """Generate a random name.
+
+    The name is composed of 2-4 name parts, followed by a numeric token.
+    The parts are seprated by a delimiter.
+
+    Args:
+        delimiter: The delimiter that separates the name parts.
+            Could be arbitrary-length string.
+        token_len: The length in digits of the numeric suffix.
+            For example, 4  yields '0000' - '9999'.
+
+    Returns:
+        A new random name.
+    """
     items = [random.choice(FIRST_NAMES).replace('_', delimiter),
              random.choice(LAST_NAMES).replace('_', delimiter)]
     if token_len > 0:
