c810d279f7b9f59130a424121edf09f91df88bea
Elad Kehat <eladkehat@gmail.com>
2019-04-13 22:40:06 +0300

    Added the option to specify the numeric token.

--- tests/test_baconator.py
+++ tests/test_baconator.py
@@ -21,3 +21,13 @@ def test_generate_with_custom_token_len():
 def test_generate_without_token():
     name = baconator.generate(token_len=0)
     assert re.fullmatch(r'[a-z]+\-[a-z]+(\-[a-z])?', name, re.I)
+
+
+def test_generate_with_specific_token():
+    # token_len=0, so still no token
+    name = baconator.generate(token_len=0, token=42)
+    assert re.fullmatch(r'[a-z]+\-[a-z]+(\-[a-z])?', name, re.I)
+    name = baconator.generate(token_len=1, token=42)
+    assert re.fullmatch(r'([a-z]+\-){2,3}42', name, re.I)
+    name = baconator.generate(token_len=4, token=42)
+    assert re.fullmatch(r'([a-z]+\-){2,3}0042', name, re.I)
